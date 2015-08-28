eb2ba8d48290c0304b4e6e8b0e57e5e1de5b1dbb
njanetos <nejanetos@gmail.com>
2015-08-28 18:10:07 -0400

    Implemented nodes.

--- include/c_node.h
+++ include/c_node.h
@@ -2,6 +2,7 @@
 #define C_NODE_H
 
 #include <string>
+#include <sstream>
 
 #include "common.h"
 #include "c_tikz_obj.h"
@@ -10,11 +11,13 @@ class c_node : public c_tikz_obj
 {
     public:
         c_node();
+        c_node(real x, real y, std::string text);
         virtual ~c_node();
 
         std::string write(std::string params);
         c_tikz_obj* project(c_camera *cam);
         c_tikz_obj* clone();
+        real x, y;
     protected:
     private:
         std::string text;
--- src/c_node.cpp
+++ src/c_node.cpp
@@ -2,23 +2,30 @@
 
 #include "c_camera.h"
 
-c_node::c_node()
-{
+c_node::c_node() {
     //ctor
 }
 
-c_node::~c_node()
-{
-    //dtor
+c_node::c_node(real x, real y, std::string text) {
+    this->x = x;
+    this->y = y;
+    this->text = text;
 }
 
+c_node::~c_node() {
+    //dtor
+}
 
 c_tikz_obj* c_node::project(c_camera *cam) {
     return cam->project(this);
 }
 
 std::string c_node::write(std::string params) {
-    return "";
+    std::stringstream sstm;
+
+    sstm << "\\node[" << params << "] at (" << x << ", " << y << ") {" << text << "};\n";
+
+    return sstm.str();
 }
 
 c_tikz_obj* c_node::clone() {
