19d59573f7d9a00f9174ec8fb842347c0da212da
njanetos <nejanetos@gmail.com>
2015-08-28 18:15:18 -0400

    Added polygons.

--- include/c_polygon.h
+++ include/c_polygon.h
@@ -3,16 +3,28 @@
 
 #include "common.h"
 #include "c_tikz_obj.h"
+#include "c_point.h"
 
 class c_polygon : public c_tikz_obj
 {
     public:
         c_polygon();
+        c_polygon(real ax,
+                  real ay,
+                  real az,
+                  real bx,
+                  real by,
+                  real bz,
+                  real cx,
+                  real cy,
+                  real cz);
         virtual ~c_polygon();
 
         std::string write(std::string params);
         c_tikz_obj* project(c_camera *cam);
         c_tikz_obj* clone();
+
+        c_point a, b, c;
     protected:
     private:
 };
--- src/c_polygon.cpp
+++ src/c_polygon.cpp
@@ -2,13 +2,33 @@
 
 #include "c_camera.h"
 
-c_polygon::c_polygon()
-{
+c_polygon::c_polygon() {
     //ctor
 }
 
-c_polygon::~c_polygon()
-{
+c_polygon::c_polygon(real ax,
+                     real ay,
+                     real az,
+                     real bx,
+                     real by,
+                     real bz,
+                     real cx,
+                     real cy,
+                     real cz) {
+
+    a.x = ax;
+    a.y = ay;
+    a.z = az;
+    b.x = bx;
+    b.y = by;
+    b.z = bz;
+    c.x = cx;
+    c.y = cy;
+    c.z = cz;
+
+}
+
+c_polygon::~c_polygon() {
     //dtor
 }
 
@@ -18,7 +38,11 @@ c_tikz_obj* c_polygon::project(c_camera *cam) {
 }
 
 std::string c_polygon::write(std::string params) {
-    return "";
+    std::stringstream sstm;
+
+    sstm << "\\fill[" << params << "] (" << a.x << ", " << a.y << ") -- (" << b.x << ", " << b.y << ") -- (" << c.x << ", " << c.y << ") -- (" << a.x << ", " << a.y << ");\n";
+
+    return sstm.str();
 }
 
 c_tikz_obj* c_polygon::clone() {
